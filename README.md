665a755c09e5f5d45f2e615ac1bfa3ae22c133a2
Kaviyarasu <kaviyarasu@meliosystems.com>
2018-08-01 14:54:42 +0530

    added basic upload and download file sample

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# ipfs-java-example
+# Simple IPFS JAVA Example
+
+#### Install IPFS
+
+https://ipfs.io/docs/install/
+
+#### IPFS getting started
+
+https://ipfs.io/docs/getting-started/
+
+#### Running Example
+
+mvn clean install
+
+mvn clean package 
+
+java -classpath target\ipfs-java-example-1.0-SNAPSHOT.jar com.clustrex.app.App
--- src/main/java/com/clustrex/app/App.java
+++ src/main/java/com/clustrex/app/App.java
@@ -1,13 +1,42 @@
 package com.clustrex.app;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import io.ipfs.api.IPFS;
+import io.ipfs.api.MerkleNode;
+import io.ipfs.api.NamedStreamable;
+import io.ipfs.multiaddr.MultiAddress;
+import io.ipfs.multihash.Multihash;
+
 /**
- * Hello world!
+ * Simple IPFS Upload!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+    	
+    	IPFS ipfs = new IPFS(new MultiAddress("/ip4/127.0.0.1/tcp/5001"));
+    	
+    	// replace your file name
+    	NamedStreamable.FileWrapper file = new NamedStreamable.FileWrapper(new File("C:\\Users\\Murugesh\\Downloads\\bfitlogo.png"));
+    	List<MerkleNode> addResult = ipfs.add(file);
+    	
+    	
+    	//Open your browser and copy past this url  
+    	System.out.println("http://localhost:8080/ipfs/" + addResult.get(addResult.size() - 1).hash.toBase58());
+    	
+    	// Getting the same hash's content back from IPFS and store as output.png
+    	Multihash filePointer = Multihash.fromBase58(addResult.get(addResult.size() - 1).hash.toBase58());
+    	byte[] fileContents = ipfs.cat(filePointer);
+
+    	try (FileOutputStream fos = new FileOutputStream("C:\\\\Users\\\\Murugesh\\\\Downloads\\\\ooutput.png")) {
+    		   fos.write(fileContents);
+    		}
+    	
     }
 }
