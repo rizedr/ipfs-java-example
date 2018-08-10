a4c1114064565633cf69f695390bbe3e25c7add9
Kaviyarasu <kaviyarasu@meliosystems.com>
2018-08-10 14:46:45 +0530

    added ssl support and Viant node integration

--- src/main/java/com/clustrex/app/App.java
+++ src/main/java/com/clustrex/app/App.java
@@ -8,8 +8,11 @@ import java.util.List;
 import io.ipfs.api.IPFS;
 import io.ipfs.api.MerkleNode;
 import io.ipfs.api.NamedStreamable;
-import io.ipfs.multiaddr.MultiAddress;
+import io.ipfs.*;
+import io.ipfs.api.*;
+import io.ipfs.api.cbor.*;
 import io.ipfs.multihash.Multihash;
+import io.ipfs.multiaddr.*;
 
 /**
  * Simple IPFS Upload!
@@ -20,23 +23,16 @@ public class App
     public static void main( String[] args ) throws IOException
     {
     	
-    	IPFS ipfs = new IPFS(new MultiAddress("/ip4/127.0.0.1/tcp/5001"));
+    	IPFS ipfs = new IPFS("develop.supplychain.consensys.net",443,"/ipfs/api/v0/",true);
+    	System.out.println(ipfs.host+ipfs.port);
     	
-    	// replace your file name
+    	
     	NamedStreamable.FileWrapper file = new NamedStreamable.FileWrapper(new File("C:\\Users\\Murugesh\\Downloads\\bfitlogo.png"));
     	List<MerkleNode> addResult = ipfs.add(file);
     	
     	
     	//Open your browser and copy past this url  
     	System.out.println("http://localhost:8080/ipfs/" + addResult.get(addResult.size() - 1).hash.toBase58());
-    	
-    	// Getting the same hash's content back from IPFS and store as output.png
-    	Multihash filePointer = Multihash.fromBase58(addResult.get(addResult.size() - 1).hash.toBase58());
-    	byte[] fileContents = ipfs.cat(filePointer);
-
-    	try (FileOutputStream fos = new FileOutputStream("C:\\\\Users\\\\Murugesh\\\\Downloads\\\\ooutput.png")) {
-    		   fos.write(fileContents);
-    		}
     	
     }
 }
