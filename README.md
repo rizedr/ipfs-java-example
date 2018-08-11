c725384461390bc30350d2d72bee83bba785fd00
kaviyarasu7 <arasu.kavi72001@gmail.com>
2018-08-11 12:38:47 +0530

    Update Argument based File upload

--- README.md
+++ README.md
@@ -8,18 +8,10 @@ https://ipfs.io/docs/install/
 
 https://ipfs.io/docs/getting-started/
 
-#### Runnig IPFS 
-
-ipfs init
-
-ipfs daemon
-
 #### Running Example
-
-change input and output file location in src/main/java/com/clustrex/app/App.java
 
 mvn clean install
 
 mvn clean package 
 
-java -classpath target\ipfs-java-example-1.0-SNAPSHOT.jar com.clustrex.app.App
+java -classpath target\ipfs-java-example-1.0-SNAPSHOT.jar com.clustrex.app.App uploadFilePath="XXX" ipfsHash="YYY" downloadFilePath="ZZZ"
--- src/main/java/com/clustrex/app/App.java
+++ src/main/java/com/clustrex/app/App.java
@@ -5,8 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 import io.ipfs.api.IPFS;
 import io.ipfs.api.MerkleNode;
@@ -25,26 +24,40 @@ public class App
 {
     public static void main( String[] args ) throws IOException
     {
-    	
-    	InputStream s = App.class.getResourceAsStream("/config.properties");
 
-    	//now can use this input stream as usually, i.e. to load as properties
+			// get the input arguments
+    	String getFilePath = args[0].substring(15);
+	    String IPFSPath = args[1].substring(9);
+	    String saveFilePath = args[2].substring(17);
+
+    	InputStream s = App.class.getResourceAsStream("/config.properties");
+    	// now can use this input stream as usually, i.e. to load as properties
     	Properties props = new Properties();
-    	
-		props.load(s);
-    	
-		System.out.println(props.getProperty("IPFS_HOST"));
-		
-    	IPFS ipfs = new IPFS("develop.supplychain.consensys.net",443,"/ipfs/api/v0/",true);
-    	System.out.println(ipfs.host+ipfs.port);
-    	
-    	
-    	NamedStreamable.FileWrapper file = new NamedStreamable.FileWrapper(new File("C:\\Users\\Murugesh\\Downloads\\bfitlogo.png"));
+			props.load(s);
+			// get the property value
+			String ipfsHost = props.getProperty("IPFS_HOST");
+			Integer apiPort = Integer.valueOf(props.getProperty("IPFS_API_PORT"));
+			String apiPath = props.getProperty("IPFS_API_PATH");
+			String ipfsProtocol = props.getProperty("IPFS_PROTOCOL");
+			String ipfsGatewayPath = props.getProperty("IPFS_GATEWAY_PATH");
+
+	     IPFS ipfs = new IPFS(ipfsHost,apiPort,"/"+apiPath,true);
+
+    	// replace your file name
+    	NamedStreamable.FileWrapper file = new NamedStreamable.FileWrapper(new File(getFilePath));
     	List<MerkleNode> addResult = ipfs.add(file);
     	
     	
     	//Open your browser and copy past this url  
-    	System.out.println("http://localhost:8080/ipfs/" + addResult.get(addResult.size() - 1).hash.toBase58());
+    	System.out.println("File has been successfully uploaded to ipfs, To view the file "+ ipfsProtocol + "//" + ipfsHost + ipfsGatewayPath + addResult.get(addResult.size() - 1).hash.toBase58());
     	
+    	// Getting the same hash's content back from IPFS and store as output.png
+    	Multihash filePointer = Multihash.fromBase58(IPFSPath);
+    	byte[] fileContents = ipfs.cat(filePointer);
+
+    	try (FileOutputStream fos = new FileOutputStream(saveFilePath)) {
+    		   fos.write(fileContents);
+    		   System.out.println("File Successfully saved in your current location");
+    		}
     }
 }
