ab207c8522fffb855c0662556a71d5a321232e12
kaviyarasu7 <arasu.kavi72001@gmail.com>
2018-08-13 12:14:07 +0530

    Add file upload and get file as seperate operation - Update Readme

--- README.md
+++ README.md
@@ -14,4 +14,18 @@ mvn clean install
 
 mvn clean package 
 
-java -classpath target\ipfs-java-example-1.0-SNAPSHOT.jar com.clustrex.app.App uploadFilePath="XXX" ipfsHash="YYY" downloadFilePath="ZZZ"
+To connect to Viant IPFS in Java we need SSL authentication, below steps helps to get and set the certificate for the domain.
+
+1. Download SSL from https://www.openssl.org if SSL is not in your system.
+2. Move to the SSL location in CLI tool and connect to viant server using the command _openssl s_client -connect develop.supplychain.consensys.net:443 -servername develop.supplychain.consensys.net > develop.supplychain.consensys.net_ssl_cert.pem_
+3. Add the created pem file to your keystore using the command _keytool -import -keystore cacerts -alias devviant -file "C:\OpenSSL-Win64\bin\develop.supplychain.consensys.net_ssl_cert.pem"
+
+##### Upload File
+java -classpath target\ipfs-java-example-1.0-SNAPSHOT.jar com.clustrex.app.App -Djavax.net.ssl.trustStore="SSL_LOCATION" -Djavax.net.ssl.trustStorePassword="SSL_PASSWORD" uploadFilePath="FILE_LOCATION_PATH"
+
+Example: java -classpath target\ipfs-java-example-1.0-SNAPSHOT.jar com.clustrex.app.App -Djavax.net.ssl.trustStore="C:\Users\Murugesh\Desktop\Projects\java-ipfs-api\cacerts" -Djavax.net.ssl.trustStorePassword="password123" uploadFilePath="C:/Users/Clustrex4/Downloads/2.png"
+
+##### Download File
+java -classpath target\ipfs-java-example-1.0-SNAPSHOT.jar com.clustrex.app.App -Djavax.net.ssl.trustStore="SSL_LOCATION" -Djavax.net.ssl.trustStorePassword="SSL_PASSWORD" ipfsHash="IPFS_HASH" downloadFilePath="FILE_DOWNLOAD_LOCATION_PATH"
+
+Example: java -classpath target\ipfs-java-example-1.0-SNAPSHOT.jar com.clustrex.app.App -Djavax.net.ssl.trustStore="C:\Users\Murugesh\Desktop\Projects\java-ipfs-api\cacerts" -Djavax.net.ssl.trustStorePassword="password123" ipfsHash="QmXfvS2aMRmJKXhnYg7zT9qHN6BmgViJn4cMSfnY1H4Qmo" downloadFilePath="C:/Users/Clustrex4/Downloads/2.png"
--- src/main/java/com/clustrex/app/App.java
+++ src/main/java/com/clustrex/app/App.java
@@ -25,10 +25,25 @@ public class App
     public static void main( String[] args ) throws IOException
     {
 
-			// get the input arguments
-    	String getFilePath = args[0].substring(15);
-	    String IPFSPath = args[1].substring(9);
-	    String saveFilePath = args[2].substring(17);
+			String getFilePath = null;
+			String IPFSPath = null;
+			String saveFilePath = null;
+
+			for (Integer index=0; index < args.length; index++) {
+				String [] arrOfStr = args[index].split("=");
+				String firstString = arrOfStr[0];        
+				switch (firstString) {
+        case "uploadFilePath":
+            getFilePath = arrOfStr[1];
+            break;
+        case "ipfsHash":
+            IPFSPath = arrOfStr[1];
+            break;
+        case "downloadFilePath":
+            saveFilePath = arrOfStr[1];
+            break;
+        }
+			}
 
     	InputStream s = App.class.getResourceAsStream("/config.properties");
     	// now can use this input stream as usually, i.e. to load as properties
@@ -41,17 +56,16 @@ public class App
 			String ipfsProtocol = props.getProperty("IPFS_PROTOCOL");
 			String ipfsGatewayPath = props.getProperty("IPFS_GATEWAY_PATH");
 
-	     IPFS ipfs = new IPFS(ipfsHost,apiPort,"/"+apiPath,true);
+	    IPFS ipfs = new IPFS(ipfsHost,apiPort,"/"+apiPath,true);
 
-    	// replace your file name
+			if (getFilePath != null) {
     	NamedStreamable.FileWrapper file = new NamedStreamable.FileWrapper(new File(getFilePath));
     	List<MerkleNode> addResult = ipfs.add(file);
-    	
-    	
-    	//Open your browser and copy past this url  
-    	System.out.println("File has been successfully uploaded to ipfs, To view the file "+ ipfsProtocol + "//" + ipfsHost + ipfsGatewayPath + addResult.get(addResult.size() - 1).hash.toBase58());
-    	
-    	// Getting the same hash's content back from IPFS and store as output.png
+    	System.out.println("File has been successfully uploaded to ipfs, To view the file "+ ipfsProtocol + "://" + ipfsHost + "/" + ipfsGatewayPath + addResult.get(addResult.size() - 1).hash.toBase58());
+			}
+
+    	if ((IPFSPath != null) && (saveFilePath != null)) {
+				// Getting the same hash's content back from IPFS and store as output.png
     	Multihash filePointer = Multihash.fromBase58(IPFSPath);
     	byte[] fileContents = ipfs.cat(filePointer);
 
@@ -59,5 +73,6 @@ public class App
     		   fos.write(fileContents);
     		   System.out.println("File Successfully saved in your current location");
     		}
+			}
     }
 }
