2493dc964a61ea446bc157cbdbc55fb7a7c411b9
Ram-Clustrex <pram@clustrex.com>
2018-08-13 15:53:22 +0530

    Update code documentation and Readme

--- README.md
+++ README.md
@@ -1,29 +1,43 @@
 # Simple IPFS JAVA Example
 
-#### Install IPFS
+## Getting started in IPFS
 
-https://ipfs.io/docs/install/
+  To Understand the usage of IPFS go to this [link](https://ipfs.io/docs/getting-started/)
 
-#### IPFS getting started
+## Running this Example
 
-https://ipfs.io/docs/getting-started/
+- Clone this repository and Open Terminal
 
-#### Running Example
+- Run - `mvn clean install`
 
-- mvn clean install
-
-- mvn clean package 
+- And then run - `mvn clean package`
 
 - To connect to Viant IPFS in Java we need SSL authentication, below steps helps to get and set the certificate for the domain.
 
-1. Download SSL from https://www.openssl.org if SSL is not in your system.
-2. Move to the SSL location in CLI tool and connect to viant server using the command _openssl s_client -connect develop.supplychain.consensys.net:443 -servername develop.supplychain.consensys.net > develop.supplychain.consensys.net_ssl_cert.pem_
-3. Add the created pem file to your keystore using the command _keytool -import -keystore cacerts -alias devviant -file "C:\OpenSSL-Win64\bin\develop.supplychain.consensys.net_ssl_cert.pem"
+  1. If SSL is not in your system, Download SSL from [Open SSL](https://www.openssl.org).
+  2. Move to the SSL location in CLI tool and connect to viant server using the command
 
-- Upload and Download File
+      ```bash
+      openssl s_client -connect develop.supplychain.consensys.net:443 -servername develop.supplychain.consensys.net > develop.supplychain.consensys.net_ssl_cert.pem
+      ```
 
+  3. Add the created pem file to your keystore using the command
+      ```bash
+      keytool -import -keystore cacerts -alias devviant -file "C:\OpenSSL-Win64\bin\develop.supplychain.consensys.net_ssl_cert.pem"
+      ```
+
+- To Upload and Download File,
+
+  **Command**
+
+  ```Java
   java -classpath target\ipfs-java-example-1.0-SNAPSHOT.jar com.clustrex.app.App -Djavax.net.ssl.trustStore="SSL_LOCATION" -Djavax.net.ssl.trustStorePassword="SSL_PASSWORD" uploadFilePath="FILE_LOCATION_PATH" downloadFilePath="FILE_DOWNLOAD_LOCATION_PATH"
+  ```
 
   **Example**
->			java -classpath target\ipfs-java-example-1.0-SNAPSHOT.jar com.clustrex.app.App -Djavax.net.ssl.trustStore="C:\Users\Murugesh\Desktop\Projects\java-ipfs-api\cacerts" -Djavax.net.ssl.trustStorePassword="password123" uploadFilePath="C:/Users/Clustrex4/Downloads/2.png"
-downloadFilePath="C:/Users/Clustrex4/Downloads/2.png"
+
+  ```Java
+  java -classpath target\ipfs-java-example-1.0-SNAPSHOT.jar com.clustrex.app.App
+  -Djavax.net.ssl.trustStore="C:\Users\Murugesh\Desktop\Projects\java-ipfs-api\cacerts" -Djavax.net.ssl.trustStorePassword="password123" uploadFilePath="C:/Users/Clustrex4/Downloads/2.png"
+  downloadFilePath="C:/Users/Clustrex4/Downloads/2.png"
+  ```
--- src/main/java/com/clustrex/app/App.java
+++ src/main/java/com/clustrex/app/App.java
@@ -17,62 +17,63 @@ import io.ipfs.multihash.Multihash;
 import io.ipfs.multiaddr.*;
 
 /**
- * Simple IPFS Upload!
- *
+ * Simple IPFS Upload and Retrival
  */
 public class App 
 {
-    public static void main( String[] args ) throws IOException
-    {
-
-			String getFilePath = null;
-			String saveFilePath = null;
-      List<MerkleNode> addResult = null;
-			for (Integer index=0; index < args.length; index++) {
-				String [] arrOfStr = args[index].split("=");
-				String firstString = arrOfStr[0];        
-		switch (firstString) {
-        case "uploadFilePath":
-            getFilePath = arrOfStr[1];
-            break;
-        case "downloadFilePath":
-            saveFilePath = arrOfStr[1];
-            break;
-        }
+    public static void main( String[] args ) throws IOException {
+		String getFilePath = null;
+		String saveFilePath = null;
+		List<MerkleNode> addResult = null;
+		
+		// parse the arguments from command line
+		for (Integer index=0; index < args.length; index++) {
+			String [] arrOfStr = args[index].split("=");
+			String firstString = arrOfStr[0];        
+			switch (firstString) {
+				case "uploadFilePath":
+					getFilePath = arrOfStr[1];
+					break;
+				case "downloadFilePath":
+					saveFilePath = arrOfStr[1];
+					break;
 			}
+		}
 
     	InputStream s = App.class.getResourceAsStream("/config.properties");
     	// now can use this input stream as usually, i.e. to load as properties
     	Properties props = new Properties();
-			props.load(s);
-			// get the property value
-			String ipfsHost = props.getProperty("IPFS_HOST");
-			Integer apiPort = Integer.valueOf(props.getProperty("IPFS_API_PORT"));
-			String apiPath = props.getProperty("IPFS_API_PATH");
-			String ipfsProtocol = props.getProperty("IPFS_PROTOCOL");
-			String ipfsGatewayPath = props.getProperty("IPFS_GATEWAY_PATH");
-      String[] splittedPath =  getFilePath.split("/");
-      Integer uploadStringLength = splittedPath.length;
-      String uploadFileName = splittedPath[uploadStringLength-1 ];
+		props.load(s);
+		// get the property values
+		String ipfsHost = props.getProperty("IPFS_HOST");
+		Integer apiPort = Integer.valueOf(props.getProperty("IPFS_API_PORT"));
+		String apiPath = props.getProperty("IPFS_API_PATH");
+		String ipfsProtocol = props.getProperty("IPFS_PROTOCOL");
+		String ipfsGatewayPath = props.getProperty("IPFS_GATEWAY_PATH");
 
-	    IPFS ipfs = new IPFS(ipfsHost,apiPort,"/"+apiPath,true);
+		String[] splittedPath =  getFilePath.split("/");
+		Integer uploadStringLength = splittedPath.length;
+		String uploadFileName = splittedPath[uploadStringLength-1 ];
 
-			if (getFilePath != null) {
-    	NamedStreamable.FileWrapper file = new NamedStreamable.FileWrapper(new File(getFilePath));
-    	addResult = ipfs.add(file, true);
-    	System.out.println("File has been successfully uploaded to ipfs..!");
-      System.out.println("View Directory: "+ ipfsProtocol + "://" + ipfsHost + "/" + ipfsGatewayPath + addResult.get(addResult.size() - 1).hash.toBase58());
-      System.out.println("View File: "+ ipfsProtocol + "://" + ipfsHost + "/" + ipfsGatewayPath + addResult.get(addResult.size() - 1).hash.toBase58() + "/" + uploadFileName);
-			}
+		// Initiate IPFS object
+	    IPFS ipfs = new IPFS(ipfsHost, apiPort, "/" + apiPath, true);
+
+		if (getFilePath != null) {
+			NamedStreamable.FileWrapper file = new NamedStreamable.FileWrapper(new File(getFilePath));
+			addResult = ipfs.add(file, true);
+			System.out.println("File has been successfully uploaded to ipfs..!");
+			System.out.println("View Directory: "+ ipfsProtocol + "://" + ipfsHost + "/" + ipfsGatewayPath + addResult.get(addResult.size() - 1).hash.toBase58());
+			System.out.println("View File: "+ ipfsProtocol + "://" + ipfsHost + "/" + ipfsGatewayPath + addResult.get(addResult.size() - 1).hash.toBase58() + "/" + uploadFileName);
+		}
 
     	if (saveFilePath != null) {
-      MerkleNode filePart = addResult.get(0);
-      byte[] fileContents = ipfs.cat(filePart.hash);
+			MerkleNode filePart = addResult.get(0);
+			byte[] fileContents = ipfs.cat(filePart.hash);
 
-    	try (FileOutputStream fos = new FileOutputStream(saveFilePath)) {
-    		   fos.write(fileContents);
-    		   System.out.println("File Successfully saved in " + saveFilePath +" location");
-    		}
+			try (FileOutputStream fos = new FileOutputStream(saveFilePath)) {
+				fos.write(fileContents);
+				System.out.println("File Successfully saved in " + saveFilePath +" location");
 			}
+		}
     }
 }
